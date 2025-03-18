5181b77b9e2800e7453c3786b47bd0a0072a6094
MenathNDGD <nadungmenath1@gmail.com>
2025-03-18 18:04:24 +0530

    Implement deleteProduct function with error handling and response for product deletion

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -75,4 +75,25 @@ export const updateProduct = async (req, res) => {
   }
 };
 
-export const deleteProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await sql`
+      DELETE FROM products 
+      WHERE id = ${id} 
+      RETURNING *
+    `;
+
+    if (deletedProduct.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, data: deletedProduct[0] });
+  } catch (error) {
+    console.log("Error in deleteProduct", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
