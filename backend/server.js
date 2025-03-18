8ccdeec7ea6f0d8a37bd5f4da98c0dfcbd775c93
MenathNDGD <nadungmenath1@gmail.com>
2025-03-18 11:35:29 +0530

    Add middleware for security and logging in Express server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
