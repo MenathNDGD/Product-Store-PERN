12f7a9b5e0e2134e8e2b2584e82f1b6479347416
MenathNDGD <nadungmenath1@gmail.com>
2025-03-18 11:52:03 +0530

    Add product routes for retrieving and creating products

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+
+router.post("/", createProduct);
+
+export default router;
