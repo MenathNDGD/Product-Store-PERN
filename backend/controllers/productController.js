10051e030e464400d06646f3984960797d83c128
MenathNDGD <nadungmenath1@gmail.com>
2025-03-18 17:49:15 +0530

    Implement createProduct function with validation and error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,7 +11,25 @@ export const getProducts = async (req, res) => {
   }
 };
 
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price, image } = req.body;
+
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const newProduct =
+      await sql`INSERT INTO products (name, price, image) VALUES (${name}, ${price}, ${image}) RETURNING *`;
+
+    res.status(201).json({ success: true, data: newProduct[0] });
+  } catch (error) {
+    console.log("Error in createProduct", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
 export const getProduct = async (req, res) => {};
 
