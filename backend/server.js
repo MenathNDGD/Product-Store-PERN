1ce317d8320b3c84d332306ada0e57503bde4330
MenathNDGD <nadungmenath1@gmail.com>
2025-03-18 12:42:21 +0530

    Initialize database and create products table on server start

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { sql } from "./config/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -18,6 +20,25 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function initDB() {
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS products (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      image VARCHAR(255) NOT NULL,
+      price DECIMAL(10, 2) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `;
+    console.log("Database is connected");
+  } catch (error) {
+    console.log("Error while connecting to the database", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
