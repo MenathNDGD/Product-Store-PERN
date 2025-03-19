85c4811f922fc28b21eccd78622de796bdb8437c
MenathNDGD <nadungmenath1@gmail.com>
2025-03-19 14:31:55 +0530

    Add deleteProduct functionality to useProductStore hook with success and error notifications

--- frontend/src/hooks/useProductStore.jsx
+++ frontend/src/hooks/useProductStore.jsx
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const BASE_URL = "http://localhost:3000";
 
@@ -10,7 +11,6 @@ export const useProductStore = create((set, get) => ({
 
   fetchProducts: async () => {
     set({ loading: true });
-
     try {
       const response = await axios.get(`${BASE_URL}/api/products`);
       set({ products: response.data.data, error: null });
@@ -18,6 +18,23 @@ export const useProductStore = create((set, get) => ({
       if (err.status == 429)
         set({ error: "Rate limit exceeded", products: [] });
       else set({ error: "An error occurred", products: [] });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  deleteProduct: async (id) => {
+    set({ loading: true });
+    try {
+      await axios.delete(`${BASE_URL}/api/products/${id}`);
+      set((prev) => ({
+        products: prev.products.filter((product) => product.id !== id),
+        error: null,
+      }));
+      toast.success("Product deleted successfully");
+    } catch (err) {
+      console.log("Error deleting product", err);
+      toast.err("Product delete failed");
     } finally {
       set({ loading: false });
     }
