c73337056e470a2d2c3ea76cd4431b7c295c6090
MenathNDGD <nadungmenath1@gmail.com>
2025-03-19 10:43:33 +0530

    Set theme to 'forest' in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,10 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
       <Navbar />
 
       <Routes>
