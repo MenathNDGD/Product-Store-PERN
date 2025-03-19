e5634029c5b8e773b2fb3b3460286c4d59e44690
MenathNDGD <nadungmenath1@gmail.com>
2025-03-19 09:01:26 +0530

    Implement Arcjet middleware for bot detection and rate limiting

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { aj } from "./lib/arcjet.js";
 import { sql } from "./config/db.js";
+
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -16,6 +18,38 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ message: "Too many requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ message: "Bot access denied" });
+      } else {
+        res.status(403).json({ message: "Forbidden" });
+      }
+      return;
+    }
+
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ message: "Spoofed bot detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
