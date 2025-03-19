71e319571a9640b9aa960dd16cbca982fc18bef8
MenathNDGD <nadungmenath1@gmail.com>
2025-03-19 10:07:44 +0530

    Implement routing with HomePage and ProductPage components in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="text-blue-600">App</div>;
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
