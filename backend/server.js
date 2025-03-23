e5ecd195940f4e3cadbaeb44cd4cf85c1e9b9500
MenathNDGD <nadungmenath1@gmail.com>
2025-03-23 18:40:18 +0530

    Add production static file serving and disable CSP in helmet middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import { aj } from "./lib/arcjet.js";
 import { sql } from "./config/db.js";
@@ -13,10 +14,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 app.use(morgan("dev"));
 
 app.use(async (req, res, next) => {
@@ -52,6 +56,14 @@ app.use(async (req, res, next) => {
 });
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 async function initDB() {
   try {
