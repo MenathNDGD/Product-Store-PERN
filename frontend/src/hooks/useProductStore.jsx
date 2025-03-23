2675f4eda20e0ddae4b4280e5287940628f39020
MenathNDGD <nadungmenath1@gmail.com>
2025-03-23 15:27:43 +0530

    Add product form handling and submission functionality in useProductStore hook

--- frontend/src/hooks/useProductStore.jsx
+++ frontend/src/hooks/useProductStore.jsx
@@ -8,6 +8,33 @@ export const useProductStore = create((set, get) => ({
   products: [],
   loading: false,
   error: null,
+  formData: {
+    name: "",
+    price: "",
+    image: "",
+  },
+
+  setFormData: (formData) => set({ formData }),
+  resetForm: () => set({ formData: { name: "", price: "", image: "" } }),
+
+  addProduct: async (e) => {
+    e.preventDefault();
+    set({ loading: true });
+
+    try {
+      const { formData } = get();
+      await axios.post(`${BASE_URL}/api/products`, formData);
+      await get().fetchProducts();
+      get().resetForm();
+      toast.success("Product added successfully");
+      document.getElementById("add_product_modal").close();
+    } catch (error) {
+      console.log("Error adding a produc", error);
+      toast.error("Product not added. Please try again");
+    } finally {
+      set({ loading: false });
+    }
+  },
 
   fetchProducts: async () => {
     set({ loading: true });
